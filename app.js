5e093e485e02b1599ac334c63479c1fb629da39a
datodev8 <dasamato8@gmail.com>
2025-09-18 00:17:37 -0500

    Se implementa una función para agregar amigos.

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [];
+
+function agregarAmigo(){
+    // Capturar nuestro "input".
+    let inputAmigo = document.getElementById('amigo');
+    
+    // Capturar el valor escrito en ese "input".
+    const amigo = inputAmigo.value;
+
+    if(amigo === ''){
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+
+    // Guardar amigo en el ARREGLO.
+    amigos.push(amigo);
+
+    // Limpiar el input.
+    document.querySelector('#amigo').value = '';
+
+    actualizarLista();
+}
